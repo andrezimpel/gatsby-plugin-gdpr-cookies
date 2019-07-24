704dbe667d5e674ed22e46a7f5a2e3528a41d46d
Andre Zimpel <andre@unknownbasics.com>
2019-07-24 21:02:26 +0200

    add the facebook pixel script by default
    tracking does not work by default. it’s just their script.
    
    Signed-off-by: Andre Zimpel <andre@unknownbasics.com>

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -1,38 +1,25 @@
 import React from 'react';
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  // if (process.env.NODE_ENV === `production`) {
-  // }
 
-
-  return setHeadComponents([
-    <script
-      key={`gatsby-plugin-gdpr-cookies`}
-      dangerouslySetInnerHTML={{
-        __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '427433987845846');
-          fbq('track', 'PageView');
-        `,
-      }}
-    />,
-  ]);
+  // add the facebook pixel script by default to the page - it does not initialize or track anything to this point. :-)
+  if (process.env.NODE_ENV === `production`) {
+    return setHeadComponents([
+      <script
+        key={`gatsby-plugin-gdpr-cookies`}
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+          `,
+        }}
+      />,
+    ]);
+  }
 };
-
-
-// <!-- Facebook Pixel Code -->
-// <script>
-//
-//   fbq('init', '427433987845846');
-//   fbq('track', 'PageView');
-// </script>
-// <noscript><img height="1" width="1" style="display:none"
-//   src="https://www.facebook.com/tr?id=427433987845846&ev=PageView&noscript=1"
-// /></noscript>
